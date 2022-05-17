3633a98d9503007262ba6de892a710b0d89b1fd6
Kanchana-at-840620226347 <xts.kanchana@gmail.com>
2022-05-17 15:19:36 +0530

    Environment Production url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,7 @@ export const environment = {
   secretKey: 'track#0071#',
   // domain: 'http://carrerportal-staging.s3-website.ap-south-1.amazonaws.com/api',
   // domain: 'https://tn.itsmine.tech/api',
-  domain: 'http://43.204.33.103/api',
+  // domain: 'http://43.204.33.103/api',
+  domain : 'https://vazhikaatti.tnschools.gov.in/api'
+
 };
