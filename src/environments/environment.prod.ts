2ab15a334757e9d8da30d948672560f28f121780
Kanchana-at-840620226347 <xts.kanchana@gmail.com>
2022-10-17 14:49:34 +0530

    / added in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   encryptedReq: false,
   secretKey: 'track#0071#',
-  domain: 'http://carrerportal-staging.s3-website.ap-south-1.amazonaws.com/api',
+  domain: 'http://carrerportal-staging.s3-website.ap-south-1.amazonaws.com/api/',
   // domain: 'https://tn.itsmine.tech/api',
   //domain: 'http://43.204.33.103/api',
   //domain: 'http://ec2-3-90-213-241.compute-1.amazonaws.com/api/api/',
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -6,6 +6,6 @@ export const environment = {
   // Local Node Environement
   // socket: 'ws://localhost:7000',
   // domain: 'http://localhost:7200/api/v1',
-  domain : 'https://vazhikaatti.tnschools.gov.in/api'
+  domain : 'https://vazhikaatti.tnschools.gov.in/api/'
 
 };
